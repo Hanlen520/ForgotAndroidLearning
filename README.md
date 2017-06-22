243a047546e1ddd33061cf16076bbdb9a9223e4d
forgot2015 <jouney90@163.com>
2017-06-23 01:18:11 +0800

    完成前两章

--- README.md
+++ README.md
@@ -2,11 +2,27 @@
 看Head First Android 做的笔记
 <br>
 ## 第一章 初尝
-
+- Android 平台框架图
+- 项目文件结构
+- Android Studio 基本使用
+- 第一个 "Hello World" 应用
+- 创建模拟器，用模拟器调试应用
+- 编译、打包、部署和运行
+- 在 Android Monitor 查看进度
+- string 字符串要在 values 下的 strings.xml 声明，一是方便多语言本地化处理，而是方便后期统一修改管理
+- 英文value文件夹是 value-en , 中文是 value-zh
 ## 第二章 构建交互
-string字符串要在values下的strings.xml声明，一是方便多语言本地化处理，而是方便后期统一修改管理
+RelativeLayout的各属性应用，如layout_alignLeft , layout_below,layout_alignParentTop
+Spinner的使用,用 getSelectedItem() 来获取当前选项
+arrays.xml的使用  string-array
+List和ArrayList的使用
+StringBuilder的使用
+foreach语句的使用
+Button点击的几种方法：
+xml布局中添加onClick, 匿名内部类方法，Activity中实现OnClickListener接口，自定义内部类方法
 
 ## 第三章 活动和意图
+ACTION_CALL
 
 ## 第四章 活动的生命周期
 
--- app/src/main/java/tech/fuge/www/forgotheadfirstandroid/activity/MainActivity.java
+++ app/src/main/java/tech/fuge/www/forgotheadfirstandroid/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package tech.fuge.www.forgotheadfirstandroid.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import tech.fuge.www.forgotheadfirstandroid.R;
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_wine_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WineRecommendActivity.class));
+            }
+        });
     }
 }
