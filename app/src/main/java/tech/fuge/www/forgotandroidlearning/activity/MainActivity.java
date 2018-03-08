3cd5f9d583473a7ed04ef8b5bba73df2ee1d1ae9
linzongfu <jouney90@gmail.com>
2018-03-08 09:46:49 +0800

    删除无用代码，添加自定义loadingDialog

--- app/src/main/java/tech/fuge/www/forgotandroidlearning/activity/MainActivity.java
+++ app/src/main/java/tech/fuge/www/forgotandroidlearning/activity/MainActivity.java
@@ -1,152 +1,94 @@
 package tech.fuge.www.forgotandroidlearning.activity;
 
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import tech.fuge.www.forgotandroidlearning.R;
 import tech.fuge.www.forgotandroidlearning.base.BaseActivity;
+import tech.fuge.www.forgotandroidlearning.view.LoadingDialog;
 
 public class MainActivity extends BaseActivity {
-    TextView mTextView;
-    Button btnPress;
+    public static final String TAG = "MainActivity";
+    @BindView(R.id.btn_dialog)
+    Button btnDialog;
+    @BindView(R.id.tv_msg)
+    TextView tvMsg;
+    @BindView(R.id.btn_thread_ui)
+    Button btnThreadUi;
+    @BindView(R.id.btn_thread_handler)
+    Button btnThreadHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //红酒推荐Activity
-        findViewById(R.id.btn_wine_activity).setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+        loadingDialog = new LoadingDialog(this).setMessage("加载中，请稍后...");
+        new Handler().post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, WineRecommendActivity.class));
+            public void run() {
+                tvMsg.setText("神荼");
             }
         });
-        //发送文本
-        findViewById(R.id.btn_action).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND)
-                        .setType("text/plain")
-                        .putExtra(Intent.EXTRA_TEXT, "发送给别人的消息");
-                startActivity(intent);
-            }
-        });
-        //选择器
-        findViewById(R.id.btn_select).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND)
-                        .setType("text/plain")
-                        .putExtra(Intent.EXTRA_TEXT, "选择器：发送给别人的消息");
-                String chosenTitle = getString(R.string.chooser);
-                Intent chosenIntent = Intent.createChooser(intent, chosenTitle);
-                startActivity(chosenIntent);
-            }
-        });
-//        testFastJson();
-        findViewById(R.id.chronograph).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ChronographActivity.class));
-            }
-        });
-        findViewById(R.id.btn_gridlayout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GridLayoutActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn_widget).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WidgetsActivity.class));
-            }
-        });
-        findViewById(R.id.list).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FoodListViewActivity.class));
-            }
-        });
-
-        findViewById(R.id.workout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WorkOutActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn_titlebar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ActionbarActivity.class));
-            }
-        });
-
-        mTextView = (TextView) findViewById(R.id.text);
-        mTextView.setText(R.string.age);
-        mTextView.setText(String.format(getString(R.string.age), 20));
-        btnPress = (Button) findViewById(R.id.btn_press);
-        btnPress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doAfterPress();
-            }
-        });
-//        String a;
-//        a.equals("ss");
-        mCanvas.drawCircle(300, 300, 200, paint);
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                tvMsg.setText("神荼222");
+            }
+        });
+    }
 
-    Paint paint = new Paint();
-    Canvas mCanvas = new Canvas();
-
-
-    void doAfterPress() {
-        System.out.println("press");
-        btnPress.setEnabled(false);
+    @OnClick(R.id.btn_dialog)
+    public void onBtnDialogClicked() {
+        loadingDialog.show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                btnPress.setEnabled(true);
+                loadingDialog.dismiss();
+                tvMsg.setText("loadingDialog");
+                Toast.makeText(MainActivity.this, "handler toast", Toast.LENGTH_SHORT).show();
             }
-        }, 1000);
+        }, 2000);
     }
-//    public static void testFastJson() {
-//        Group group = new Group();
-//        group.setId(0L);
-//        group.setName("admin");
-//
-//        User guestUser = new User();
-//        guestUser.setId(2L);
-//        guestUser.setName("guest");
-//
-//        User rootUser = new User();
-//        rootUser.setId(3L);
-//        rootUser.setName("root");
-//
-//        group.addUser(guestUser);
-//        group.addUser(rootUser);
-//
-////        String jsonString = JSON.toJSONString(group);
-//
-////        System.out.println(jsonString);
-////        Log.w("toString", jsonString);
-//
-////        Group group1 = JSON.parseObject("{\"id\":0,\"name\":\"admin\",\"users\":[{\"id\":2,\"name\":\"guest\"},{\"id\":3,\"name\":\"root\"}]}", Group.class);
-////        Log.e("tag", group.toString());
-//    }
 
+    @OnClick(R.id.btn_thread_ui)
+    public void onBtnThreadUiClicked() {
+        Log.e(TAG, "onBtnThreadUiClicked");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                tvMsg.setText("Thread update ui");
+            }
+        }).start();
+    }
 
+    @OnClick(R.id.btn_thread_handler)
+    public void onViewClicked() {
+        Log.e(TAG, "Thread Handler Update UI");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "Thread Handler Update UI111");
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e(TAG, "Thread Handler Update UI222");
+                        tvMsg.setText("Thread Handler Update UI");
+                    }
+                });
+            }
+        }).start();
+    }
 }
--- app/src/main/java/tech/fuge/www/forgotandroidlearning/base/BaseActivity.java
+++ app/src/main/java/tech/fuge/www/forgotandroidlearning/base/BaseActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import tech.fuge.www.forgotandroidlearning.BuildConfig;
 import tech.fuge.www.forgotandroidlearning.R;
+import tech.fuge.www.forgotandroidlearning.view.LoadingDialog;
 
 /**
  * Created by Lin Zongfu on 2017/6/23.
@@ -25,6 +26,8 @@ public class BaseActivity extends AppCompatActivity {
     //区别debug和release版本字段，可用来开关打印。不必每次发布前手动修改
     public final static boolean LOG_DEBUG = BuildConfig.DEBUG;
 
+    protected LoadingDialog loadingDialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,61 +35,16 @@ public class BaseActivity extends AppCompatActivity {
             Log.e(TAG, getClass().getSimpleName());
         }
         ActivityCollector.addActivity(this);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.base_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-//        return super.onOptionsItemSelected(item);
-        switch (item.getItemId()){
-            case R.id.show_activity:
-                Toast.makeText(this, ActivityCollector.showAll(), Toast.LENGTH_SHORT).show();
-                return true;
-            default:return super.onOptionsItemSelected(item);
-        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+        if (loadingDialog != null) {
+            loadingDialog.dismiss();
+        }
     }
 
-    protected void showToast(String text) {
-        Toast.makeText(getApplication(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    protected void showLongToast(String text) {
-        Toast.makeText(getApplication(), text, Toast.LENGTH_LONG).show();
-    }
-
-    protected void showSnackBar(View view, String text) {
-        Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show();
-    }
-
-    protected void showLongSnackBar(View view, String text) {
-        Snackbar.make(view, text, Snackbar.LENGTH_LONG).show();
-    }
-
-    protected void showSnackBarWithAction(View view, String text, String actionText, View.OnClickListener clickListener) {
-        Snackbar.make(view, text, Snackbar.LENGTH_SHORT).setAction(actionText, clickListener).show();
-    }
-
-    protected void showLongSnackBarWithAction(View view, String text, String actionText, View.OnClickListener clickListener) {
-        Snackbar.make(view, text, Snackbar.LENGTH_LONG).setAction(actionText, clickListener).show();
-    }
-
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        // TODO Auto-generated method stub
-//        if (keyCode == KeyEvent.KEYCODE_BACK) {//屏蔽返回键
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
 }
